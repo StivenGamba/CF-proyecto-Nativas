ee5a8e2ebc0abb50b8d5127f54fe231f5f7b6a5c
StivenGamba <130689204+StivenGamba@users.noreply.github.com>
2025-05-15 20:37:19 -0500

    Funcionando otra vez

--- app/src/main/java/com/example/taller_3_fragments/fragments/inicioFragment.kt
+++ app/src/main/java/com/example/taller_3_fragments/fragments/inicioFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.taller_3_fragments.R
 
 class inicioFragment : Fragment() {
@@ -14,5 +16,17 @@ class inicioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_inicio, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<Button>(R.id.btnIrLogin)
+        btn.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_loginFragment)
+        }
+
     }
 }
